a4651458fdf55e7dc5181316ec581f0d7e702838
Leonardo B. Hurovich <105162659+leobh76@users.noreply.github.com>
2022-06-19 17:35:56 +0300

    First commit

--- D01/EX07/index.js
+++ D01/EX07/index.js
@@ -1,7 +1,22 @@
 function getRandomInt(max) {
-  //your code
+  return Math.floor(Math.random() * max);
 }
 
 function moduloEx7() {
-  //your code
+  let num1 = getRandomInt(101);
+  let num2 = getRandomInt(101);
+  let result;
+  if (num1 > num2 && num1 % num2 !== 0) {
+    result = `The number ${num1} is bigger than ${num2}. But the modulo of ${num1} % ${num2} is ${num1 % num2}`;
+  } else if (num1 > num2) {
+    result = `The number ${num1} is bigger than ${num2}`;
+  } else if (num1 === num2) {
+    result = `The number ${num1} is equal to ${num2}`;
+  } else {
+    result = `The number ${num1} is less than ${num2}`;
+  }
+  const modulo = document.getElementById("output");
+  modulo.innerText = result;
 }
+
+moduloEx7();
